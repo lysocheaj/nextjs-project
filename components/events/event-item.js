af42fc4b62b8a1bf2ee98a538c89119cd6ce8519
Your Name <lysochea@gmail.com>
2022-06-16 16:59:49 +0700

    style

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,39 @@
-function EventItem(props) {
-    return <div>
+import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
 
-    </div>
+function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const formattedAdrr = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAdrr}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          {/* <Link href={exploreLink}>Explore Events</Link> */}
+          <Button link={exploreLink}>Explore Event</Button>
+        </div>
+      </div>
+    </li>
+  );
 }
+
+export default EventItem;
